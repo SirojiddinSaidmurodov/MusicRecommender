2fecb8392bb5b23e06ec2645ca0c4e05329e79f0
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-05-13 16:13:36 +0300

    edit recommender algo for taking in ids and giving out ids
    Took 3 minutes

--- MusicApp/recommender.py
+++ MusicApp/recommender.py
@@ -21,41 +21,17 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.environ[
 song_cluster_pipeline = load('blobs/song_cluster')
 data = pd.read_csv("blobs/data.csv")
 
-
-def find_song(name, year):
-    song_data = defaultdict()
-    results = sp.search(q='track: {} year: {}'.format(name, year), limit=1)
-    if not results['tracks']['items']:
-        return None
-
-    results = results['tracks']['items'][0]
-    track_id = results['id']
-    audio_features = sp.audio_features(track_id)[0]
-
-    song_data['name'] = [name]
-    song_data['year'] = [year]
-    song_data['explicit'] = [int(results['explicit'])]
-    song_data['duration_ms'] = [results['duration_ms']]
-    song_data['popularity'] = [results['popularity']]
-
-    for key, value in audio_features.items():
-        song_data[key] = value
-
-    return pd.DataFrame(song_data)
-
-
 number_cols = ['valence', 'year', 'acousticness', 'danceability', 'duration_ms', 'energy', 'explicit',
                'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'popularity', 'speechiness', 'tempo']
 
 
-def get_song_data(song, spotify_data):
+def get_song_data_by_id(spotify_id, spotify_data):
     try:
-        song_data = spotify_data[(spotify_data['name'] == song['name'])
-                                 & (spotify_data['year'] == song['year'])].iloc[0]
+        song_data = spotify_data[(spotify_data['id'] == spotify_id)].iloc[0]
         return song_data
 
     except IndexError:
-        return find_song(song['name'], song['year'])
+        return find_song_by_id(spotify_id)
 
 
 def find_song_by_id(spotify_id):
@@ -79,20 +55,10 @@ def find_song_by_id(spotify_id):
     return pd.DataFrame(song_data)
 
 
-def get_song_data_by_id(spotify_id, spotify_data):
-    try:
-        song_data = spotify_data[(spotify_data['id'] == spotify_id)].iloc[0]
-        return song_data
-
-    except IndexError:
-        return find_song_by_id(spotify_id)
-
-
 def get_mean_vector(song_list, spotify_data):
     song_vectors = []
-
     for song in song_list:
-        song_data = get_song_data(song, spotify_data)
+        song_data = get_song_data_by_id(song, spotify_data)
         if song_data is None:
             print('Warning: {} does not exist in Spotify or in database'.format(song['name']))
             continue
@@ -103,22 +69,12 @@ def get_mean_vector(song_list, spotify_data):
     return np.mean(song_matrix, axis=0)
 
 
-def flatten_dict_list(dict_list):
-    flattened_dict = defaultdict()
-    for key in dict_list[0].keys():
-        flattened_dict[key] = []
-
-    for dictionary in dict_list:
-        for key, value in dictionary.items():
-            flattened_dict[key].append(value)
-
-    return flattened_dict
+def recommend(song_list):
+    return recommend_songs(song_list, data, n_songs=20)
 
 
 def recommend_songs(song_list, spotify_data, n_songs=10):
-    metadata_cols = ['name', 'year', 'artists']
-    song_dict = flatten_dict_list(song_list)
-
+    metadata_cols = ['id']
     song_center = get_mean_vector(song_list, spotify_data)
     scaler = song_cluster_pipeline.steps[0][1]
     scaled_data = scaler.transform(spotify_data[number_cols])
@@ -127,8 +83,9 @@ def recommend_songs(song_list, spotify_data, n_songs=10):
     index = list(np.argsort(distances)[:, :n_songs][0])
 
     rec_songs = spotify_data.iloc[index]
-    rec_songs = rec_songs[~rec_songs['name'].isin(song_dict['name'])]
-    return rec_songs[metadata_cols].to_dict(orient='records')
+    rec_songs = rec_songs[~rec_songs['id'].isin(song_list)]
+    recsongsdict = rec_songs[metadata_cols].to_dict(orient='records')
+    return [song['id'] for song in recsongsdict]
 
 
 def search_songs(song):
@@ -136,21 +93,18 @@ def search_songs(song):
 
 
 def get_songs(songs):
-    return [sp.track(song) for song in songs]
+    if len(songs) > 0:
+        return sp.tracks(songs)
+    else:
+        return []
 
 
 if __name__ == '__main__':
+    song_cluster_pipeline = load('../blobs/song_cluster')
+    data = pd.read_csv("../blobs/data.csv")
     recommendation = recommend_songs([
-        {
-            'name': "We Don't Talk Anymore (feat. Selena Gomez)",
-            'year': 2016
-        }, {
-            "name": "Scared to Be Lonely",
-            "year": 2017
-        },
-        {
-            "name": "In My Feelings",
-            "year": 2018
-        }
+        "37FXw5QGFN7uwwsLy8uAc0",
+        "3ebXMykcMXOcLeJ9xZ17XH",
+        "2G7V7zsVDxg1yRsu7Ew9RJ"
     ], data, n_songs=20)
     print(json.dumps(recommendation, indent=4))
